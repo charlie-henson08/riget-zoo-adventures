06b4558f73d279a8b2d0b39204529693a0e2df1f
CHARLIE HENSON <24000651@student.stamford.ac.uk>
2025-12-05 15:15:02 +0000

    New Better Version

---
+++ assets/preset/header.php
@@ -0,0 +1,13 @@
+            <header>
+                <a href="index.php"><img class="logo" src="assets/images/Logo.png" alt="Site Logo"></a>
+                <nav class="tabs">
+                    <a href="plan-your-visit/index.php">Plan Your Visit</a>
+                    <a href="membership/index.php">Membership</a>
+                    <a href="education/index.php">Education</a>
+                    <a href="booking/index.php">Booking</a>
+                </nav>
+                <div class="signuplogin">
+                    <div><a href="accounts/signup.php">Sign Up</a></div>
+                    <div><a href="accounts/login.php">Log In</a></div>
+                </div>
+            </header>
--- index.php
+++ index.php
@@ -1,94 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RigetZooAdventures</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
-<body class="page">
-    <main>
-        <section>
-            <div class="hero"> 
-                <div class="heroImage">
-                    <img src="assets/images/homepage/HeroImage.png" alt="Lion">
-                    <div class="heroSlogan">
-                        <h2>Experience Riget Zoo Adventures</h2>
-                        <h1>Book Tickets Now</h1>
-                        <div class="bookNow">Book Now</div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <title>Riget-Zoo Adventures</title>
+    </head>
+
+    <body>
+        <div class="page">
+            <?php include 'assets/preset/header.php'; ?> 
+
+            <main>
+                <section>
+                    <div class="heroImage">
+                        <img src="assets/images/homepage/HeroImage.png" alt="Lion">
+                        <div class="heroSlogan">
+                            <h2>Experience Riget Zoo Adventures</h2>
+                            <h1>Book Tickets Now</h1>
+                            <div class="bookNow">Book Now</div>
+                        </div>
                     </div>
-                </div>
-                
-            </div>
-        </section>
+                </section>
 
-        <header>
-            <a href="index.php"><img class="logo" src="assets/images/preset/Logo.png" alt="Site Logo"></a>
-            <nav class="tabs">
-                <a href="plan-your-visit/index.php">Plan Your Visit</a>
-                <a href="membership/index.php">Membership</a>
-                <a href="education/index.php">Education</a>
-                <a href="booking/index.php">Booking</a>
-            </nav>
-            <div class="signuplogin">
-                <div><a href="accounts/signup.php">Sign Up</a></div>
-                <div><a href="accounts/login.php">Log In</a></div>
-            </div>
-        </header>
-        
+                <section class="main">
+                    <h1>Discover The BEST Zoo at Riget Zoo Adventures</h1>
+                    <p>Discover the best wildlife experience at Riget Zoo Adventures, where exciting animal encounters and immersive habitats create unforgettable moments for visitors of all ages. Dive into adventure and explore the wild like never before!</p>                       
+                </section>
+            </main>
 
-        <section class="homepage-text">
-            <div class="homepage-main">
-                <h1>Discover The BEST Zoo at Riget Zoo Adventures</h1>
-                <h2>Discover the best wildlife experience at Riget Zoo Adventures, where exciting animal encounters and immersive habitats create unforgettable moments for visitors of all ages. Dive into adventure and explore the wild like never before!</h2>
-            </div>
-        </section>
+            <?php include 'assets/preset/footer.php'; ?>
 
-        <section class="cards">
-            <div class="card">
-                <img class="cardimg" src="assets/images/homepage/planYourVisitImage.jpg" alt="Child Feeding Animal" width="400" height="300">
-                <h2><u>Plan Your Visit</u></h2>
-                <p>Start your adventure by planning your visit.</p>
-                <a class="next" href="/plan-your-visit/index.php"><img class="next" src="assets/images/preset/Next.png"></a>
-            </div>
-    
-            <div class="card">
-                <img class="cardimg" src="assets/images/homepage/lion.png" alt="Lion" width="400" height="300">
-                <h2><u>Animal</u></h2>
-                <p>Discover the amazing animals at our zoo.</p>
-                <a class="next" href="education/index.php"><img class="next" src="assets/images/preset/Next.png"></a>
-            </div>
-    
-            <div class="card">
-                <img class="cardimg" src="assets/images/homepage/zoothemedhotel.png" alt="Zoo Themed Hotel" width="400" height="300">
-                <h2><u>Zoo Themed Hotels</u></h2>
-                <p>Make your visit magical with an overnight stay.</p>
-                <a  href="booking/index.php"><img class="next" src="assets/images/preset/Next.png"></a>
-            </div>
-        </section>
-        
-        <footer>
-            <div class="footer-container">
-                <div>
-                    <h2>Contact Us</h2>
-                    <a href="policies/index.php">Cookie Policy</a>
-                    <br><a href="policies/index.php/#privacy">Privacy Policy</a>
-                    <br><a href="policies/index.php/#equality">Equality and Diversity Policy</a>
-                    <br><a href="policies/#terms-conditions">Terms & Conditions</a>
-                    <br><a href="policies/#modern-slavery">Modern Slavery Statement</a>
-                </div>
-                <div><img src="assets/images/preset/Logo.png"></div>
-                <div>
-                    <h2>Contact Us</h2>
-                    <a href="policies/index.php"><img src="assets/images/preset/footer/tiktok.png"></a>
-                    <a href="policies/index.php"><img src="assets/images/preset/footer/insta.png"></a>
-                    <a href="policies/index.php"><img src="assets/images/preset/footer/x.png"></a>                    
-                </div>
-            </div>
-        </footer>
-    </main>
-
-
-</body>
+        </div>
+    </body>
 </html>
